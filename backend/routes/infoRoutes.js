f8bff660fc880aeda530b5ff09a132d8025a7303
faraz-github <farazahmad.email@gmail.com>
2024-08-17 16:38:31 +0530

    Implement RBAC on info routes

--- backend/routes/infoRoutes.js
+++ backend/routes/infoRoutes.js
@@ -2,21 +2,43 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
-const { createPersonalInfo, readPersonalInfo, updatePersonalInfo, deletePersonalInfo,
-    createContactInfo, readContactInfo, updateContactInfo, deleteContactInfo,
-    createPlatformInfo, readPlatformInfo, updatePlatformInfo, deletePlatformInfo,
-    createToolInfo, readToolInfo, updateToolInfo, deleteToolInfo,
-    createPortfolioInfo, readPortfolioInfo, updatePortfolioInfo, deletePortfolioInfo
+const {
+  createPersonalInfo,
+  readPersonalInfo,
+  updatePersonalInfo,
+  deletePersonalInfo,
+  createContactInfo,
+  readContactInfo,
+  updateContactInfo,
+  deleteContactInfo,
+  createPlatformInfo,
+  readPlatformInfo,
+  updatePlatformInfo,
+  deletePlatformInfo,
+  createToolInfo,
+  readToolInfo,
+  updateToolInfo,
+  deleteToolInfo,
+  createPortfolioInfo,
+  readPortfolioInfo,
+  updatePortfolioInfo,
+  deletePortfolioInfo,
 } = require("../controllers/infoController");
 
 // Middleware
 const { protectedRoute } = require("../middlewares/authMiddleware");
+const { authorizeRole } = require("../middlewares/roleMiddleware");
 
 //----------------------------------------------------------------Info Routes - Personal
 // @desc    Create personal information
 // @route   POST /api/info/personal
 // @access  Private
-router.post("/personal", protectedRoute, createPersonalInfo);
+router.post(
+  "/personal",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  createPersonalInfo
+);
 // @desc    Read personal information
 // @route   GET /api/info/personal
 // @access  Public
@@ -24,16 +46,31 @@ router.get("/personal", readPersonalInfo);
 // @desc    Update personal information
 // @route   PATCH /api/info/personal:id
 // @access  Private
-router.patch("/personal/:id", protectedRoute, updatePersonalInfo);
+router.patch(
+  "/personal/:id",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  updatePersonalInfo
+);
 // @desc    Delete personal information
 // @route   DELETE /api/info/personal:id
 // @access  Private
-router.delete("/personal/:id", protectedRoute, deletePersonalInfo);
+router.delete(
+  "/personal/:id",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  deletePersonalInfo
+);
 //----------------------------------------------------------------Info Routes - Contact
 // @desc    Create contact information
 // @route   POST /api/info/contact
 // @access  Private
-router.post("/contact", protectedRoute, createContactInfo);
+router.post(
+  "/contact",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  createContactInfo
+);
 // @desc    Read contact information
 // @route   GET /api/info/contact
 // @access  Public
@@ -41,16 +78,31 @@ router.get("/contact", readContactInfo);
 // @desc    Update contact information
 // @route   PATCH /api/info/contact:id
 // @access  Private
-router.patch("/contact/:id", protectedRoute, updateContactInfo);
+router.patch(
+  "/contact/:id",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  updateContactInfo
+);
 // @desc    Delete contact information
 // @route   DELETE /api/info/contact:id
 // @access  Private
-router.delete("/contact/:id", protectedRoute, deleteContactInfo);
+router.delete(
+  "/contact/:id",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  deleteContactInfo
+);
 //----------------------------------------------------------------Info Routes - Platform
 // @desc    Create development platform information
 // @route   POST /api/info/platform
 // @access  Private
-router.post("/platform", protectedRoute, createPlatformInfo);
+router.post(
+  "/platform",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  createPlatformInfo
+);
 // @desc    Read development platform information
 // @route   GET /api/info/platform
 // @access  Public
@@ -58,16 +110,31 @@ router.get("/platform", readPlatformInfo);
 // @desc    Update development platform information
 // @route   PATCH /api/info/platform/:id
 // @access  Private
-router.patch("/platform/:id", protectedRoute, updatePlatformInfo);
+router.patch(
+  "/platform/:id",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  updatePlatformInfo
+);
 // @desc    Delete development platform information
 // @route   DELETE /api/info/platform/:id
 // @access  Private
-router.delete("/platform/:id", protectedRoute, deletePlatformInfo);
+router.delete(
+  "/platform/:id",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  deletePlatformInfo
+);
 //----------------------------------------------------------------Info Routes - Toolset
 // @desc    Create toolset information
 // @route   POST /api/info/tool
 // @access  Private
-router.post("/tool", protectedRoute, createToolInfo);
+router.post(
+  "/tool",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  createToolInfo
+);
 // @desc    Read toolset information
 // @route   GET /api/info/tool
 // @access  Public
@@ -75,16 +142,31 @@ router.get("/tool", readToolInfo);
 // @desc    Update toolset information
 // @route   PATCH /api/info/tool/:id
 // @access  Private
-router.patch("/tool/:id", protectedRoute, updateToolInfo);
+router.patch(
+  "/tool/:id",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  updateToolInfo
+);
 // @desc    Delete toolset information
 // @route   DELETE /api/info/tool/:id
 // @access  Private
-router.delete("/tool/:id", protectedRoute, deleteToolInfo);
+router.delete(
+  "/tool/:id",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  deleteToolInfo
+);
 //----------------------------------------------------------------Info Routes - Portfolio
 // @desc    Create portfolio information
 // @route   POST /api/info/portfolio
 // @access  Private
-router.post("/portfolio", protectedRoute, createPortfolioInfo);
+router.post(
+  "/portfolio",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  createPortfolioInfo
+);
 // @desc    Read portfolio information
 // @route   GET /api/info/portfolio
 // @access  Public
@@ -92,10 +174,20 @@ router.get("/portfolio", readPortfolioInfo);
 // @desc    Update portfolio information
 // @route   PATCH /api/info/portfolio/:id
 // @access  Private
-router.patch("/portfolio/:id", protectedRoute, updatePortfolioInfo);
+router.patch(
+  "/portfolio/:id",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  updatePortfolioInfo
+);
 // @desc    Delete portfolio information
 // @route   DELETE /api/info/portfolio/:id
 // @access  Private
-router.delete("/portfolio/:id", protectedRoute, deletePortfolioInfo);
+router.delete(
+  "/portfolio/:id",
+  protectedRoute,
+  authorizeRole("superAdmin"),
+  deletePortfolioInfo
+);
 
 module.exports = router;
