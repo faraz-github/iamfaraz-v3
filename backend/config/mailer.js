d482b65dfba522c39e9fac1d120b8d982165a9f5
faraz-github <farazahmad.email@gmail.com>
2024-08-18 01:32:41 +0530

    Fix confirmation email not sending error

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,46 +1,34 @@
 const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+// Replace with your Gmail address and app password
+const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS; // Your Gmail address
+const APP_PASSWORD = process.env.APP_PASSWORD; // The app password you generated
 
 async function sendMail(sendTo, confirmationURL) {
-    try {
+  try {
+    // Transport Setup
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: EMAIL_ADDRESS,
+        pass: APP_PASSWORD,
+      },
+    });
 
-        // Transport Setup
-        const accessToken = await oAuth2Client.getAccessToken();
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                type: "OAuth2",
-                user: "faraz01000110@gmail.com",
-                clientId: CLIENT_ID,
-                clientSecret: CLIENT_SECRET,
-                refreshToken: REFRESH_TOKEN,
-                accessToken: accessToken
-            }
-        });
+    // Mail Options
+    const mailOptions = {
+      from: `Faraz Ahmad <${EMAIL_ADDRESS}>`,
+      to: sendTo,
+      subject: "Confirmation Email", // Subject line
+      text: `Please click the link to confirm: ${confirmationURL}`, // plain text body
+      html: `Please click the link to confirm: <a href="${confirmationURL}">${confirmationURL}</a>`, // html body
+    };
 
-        // Mail Options
-        const mailOptions = {
-            from: "Faraz Ahmad <faraz01000110@gmail.com>",
-            to: sendTo,
-            subject: "Confirmation Email", // Subject line
-            text: `Please click the link to confirm: ${confirmationURL}`, // plain text body
-            html: `Please click the link to confirm: <a href="${confirmationURL}">${confirmationURL}</a>`, // html body 
-        }
-
-        const result = await transporter.sendMail(mailOptions);
-        return result;
-
-    } catch (error) {
-        return error;
-    }
+    const result = await transporter.sendMail(mailOptions);
+    return result;
+  } catch (error) {
+    return error;
+  }
 }
 
 module.exports = sendMail;
