0eb598fc97e41f1ed8aa3bf30c91fbc3593d093e
faraz-github <farazahmad.email@gmail.com>
2024-09-25 17:17:22 +0530

    Frontend: connect api to status indicator

--- frontend/src/components/Navbar/StatusIndicator.jsx
+++ frontend/src/components/Navbar/StatusIndicator.jsx
@@ -1,41 +1,95 @@
-import { Box, Grow, Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
-// TODO dynamically change status & colors based on API response
+import { Box, Stack, Typography } from "@mui/material";
 
 const StatusIndicator = () => {
-  const transitionTime = 500;
+  // state
+  const [status, setStatus] = useState("");
 
-  const [show, setShow] = useState(true);
-
+  // ---API
   useEffect(() => {
-    const interval = setInterval(() => {
-      setShow((prev) => !prev);
-    }, transitionTime);
-
-    return () => clearInterval(interval);
+    const readPersonalInfo = async () => {
+      try {
+        const response = await axios.get("/api/info/personal");
+        if (response) {
+          setStatus(response.data[0]?.status);
+          return response;
+        }
+      } catch (error) {
+        console.log(error); // Debug Log
+        if (error?.response?.data?.message) {
+          toast.error(error?.response?.data?.message);
+        }
+      }
+    };
+    readPersonalInfo();
   }, []);
+
+  const statusColor = (status) => {
+    switch (status) {
+      case "available":
+        return "success.light";
+      case "busyQuick":
+        return "secondary.light";
+      case "busyLong":
+        return "primary.light";
+      case "break":
+        return "error.dark";
+      default:
+        return "background.default";
+    }
+  };
+
+  const statusText = (status) => {
+    switch (status) {
+      case "available":
+        return "Available";
+      case "busyQuick":
+        return "Little Busy";
+      case "busyLong":
+        return "Very Busy";
+      case "break":
+        return "Break Time";
+      default:
+        return "background.default";
+    }
+  };
 
   return (
     <Box height={"100%"} display={"flex"} alignItems={"center"}>
       <Stack direction={"row"} spacing={1} alignItems={"center"}>
-        <Grow in={show} timeout={transitionTime}>
+        {status ? (
           <Box
             width={15}
             height={15}
-            bgcolor={"success.light"}
+            bgcolor={statusColor(status)}
             borderRadius="50%"
+            sx={{
+              animation: "blink 1s ease-in-out infinite",
+              "@keyframes blink": {
+                "0%": { opacity: 0 },
+                "50%": { opacity: 1 },
+                "100%": { opacity: 0 },
+              },
+            }}
           ></Box>
-        </Grow>
+        ) : null}
+
         <Box
           border={"1px solid"}
-          borderColor={"success.light"}
+          borderColor={statusColor(status)}
           py={0.5}
           px={2}
           borderRadius={(theme) => theme.shape.borderRadius}
         >
-          <Typography variant="body2" color={"primary.main"}>
-            Available for work
+          <Typography
+            variant="body2"
+            fontSize={16}
+            color={statusColor(status)}
+            mt={0.5}
+          >
+            {statusText(status)}
           </Typography>
         </Box>
       </Stack>
